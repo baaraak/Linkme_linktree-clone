22421ba7a153e5fd849624eef5eb05f18edf6b16
barak <baaraak0@gmail.com>
2021-02-23 22:10:10 +0200

    change

--- client/src/hooks/useWindowDimensions.js
+++ client/src/hooks/useWindowDimensions.js
@@ -14,9 +14,9 @@ export default function useWindowDimensions() {
   );
 
   useEffect(() => {
-    function handleResize() {
+    const handleResize = () => {
       setWindowDimensions(getWindowDimensions());
-    }
+    };
 
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
